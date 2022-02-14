1ee577d25c697cb84851d7eb4c6ef63550e545c3
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-02-14 22:00:53 +0000

    Finished home page

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			backgroundColor: Colors.lightBlue[200],
+			appBar: AppBar(
+				title: Text("Home Page")),
+				body: Center(
+					child: Column(
+					mainAxisAlignment: MainAxisAlignment.center,
+					crossAxisAlignment: CrossAxisAlignment.center,
+					children: [
+					CircleAvatar(
+						backgroundImage: AssetImage('img1.jpg'),
+						radius: 150
+					),
+					SizedBox(height: 20.0),
+					Text("Hey, My name is Christian and this app is to a basic introduction to who I am."),
+					Text(" Click on the buttons below to find out my favorite activites."),
+					SizedBox(height: 20.0),
+					ElevatedButton(
+	                onPressed: () => null,
+	                child: Text(
+	                	'Favorite Movies',
+	                	textAlign: TextAlign.center),
+	              ),
+					SizedBox(height: 20.0),
+					ElevatedButton(
+	                onPressed: () => null,
+	                child: Text('Favorite Songs'),
+	              ),
+					SizedBox(height: 20.0),
+					ElevatedButton(
+	                onPressed: () => null,
+	                child: Text('Favorite Activites'),
+	              ),
+					SizedBox(height: 20.0),
+					ElevatedButton(
+	                onPressed: () => null,
+	                child: Text('Simple Calculator'),
+	              ),
+					],
+
+				),
+			),
+		);
+	}
+}
